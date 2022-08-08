9d73351e8bd6764804a05edce60f49d014f8328e
WesleySilva99 <wesleyacad001@gmail.com>
2022-08-08 20:41:04 -0300

    Commit da tela de cadastro atual

---
+++ APP/client/tela-cadastro-test/src/App.js
@@ -0,0 +1,68 @@
+import logoTela from './assets/LOGOLATERAL.png'
+import bootstrap from './assets/bootstrap/css/bootstrap.css'
+
+function App() {
+  return (
+   <div className="container">
+     <div className="container-cadastro">
+       <div className="wrap-cadastro">
+         <form className="cadastro-form">
+
+           <span className="cadastro-form-title"> Crie sua conta</span>
+
+           <span className="cadastro-form-title">
+             <img src={logoTela} alt="logoProjeto"/>
+           </span>
+            
+            <div className="wrap-input">
+              <input type="text"/>
+               <span className="focus-input" data-placeholder="Nome Completo"/>
+            </div>
+            
+            <div className="wrap-input">
+              <input type="text"/>
+               <span className="focus-input" data-placeholder="CPF ex: 000.000.000-00"/>
+            </div>
+
+            <div className="wrap-input">
+              <input type="email"/>
+               <span className="focus-input" data-placeholder="E-mail"/>
+            </div>
+
+            <div className="wrap-input">
+              <input type="text"/>
+               <span className="focus-input" data-placeholder="Digite seu telefone"/>
+            </div>
+
+            <div className="wrap-input">
+              <input type="date"/>
+               <span className="focus-input" data-placeholder="dd/mm/aaaa"/>
+            </div>
+           
+            <div className="wrap-input">
+              <input type="password"/>
+               <span className="focus-input" data-placeholder="Senha"/>
+            </div>
+
+            <div className="wrap-input">
+              <input type="password"/>
+               <span className="focus-input" data-placeholder="Confirme sua senha"/>
+            </div>
+
+            <div> 
+            <button className='btn-sm btn-success' type="submit"> Confirmar </button>
+            </div>
+
+            <div> 
+            <button className='btn-sm btn-success' type="reset"> Cancelar </button>
+            </div>
+
+
+         </form>
+       </div>
+     </div>
+   </div>
+  );
+}
+
+export default App;
